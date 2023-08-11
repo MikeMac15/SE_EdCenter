aa6125c3006d7fd46785972b4142143aa2ceaa66
Michael McIntosh <michaeldmc58@gmail.com>
2023-08-11 13:26:30 -0700

    emailjs functional + Rboot modal

--- SEED_project/src/pages/AboutSEED/contact.jsx
+++ SEED_project/src/pages/AboutSEED/contact.jsx
@@ -1,11 +1,34 @@
-import { Form } from "react-router-dom";
+
+import { useRef, useState } from "react";
+import emailjs from '@emailjs/browser';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import '../../Styles/contact.css'
 
 export const Contact_us = () => {
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-    }
+        const [success, setSuccess] = useState(null)
+        const form = useRef();
+///// Modal
+        const [show, setShow] = useState(false);
+        const handleClose = () => setShow(false);
+        // const handleShow = () => setShow(true);
+///// Modal     
+        const sendEmail = (e) => {
+          e.preventDefault();
+      
+          emailjs.sendForm('service_zp94zsn', 'template_jw6q8gd', form.current, 'zB32I6I3ggkV8vlA2')
+            .then((result) => {
+                console.log(result.text);
+                setShow(true)
+                setSuccess(true)
+            }, (error) => {
+                console.log(error.text);
+                setSuccess(false)
+            });
+        };
+
+    
 
     return (
         <>
@@ -19,31 +42,43 @@ export const Contact_us = () => {
                 <div className="conBoxBody">
                     <div className="conBodyLeft">
                         <h2>Send us a message.</h2>
-                        <Form>
+                        <form ref={form} onSubmit={sendEmail}>
                             <div className="inputRow">
                                 <div className="inputGroup">
                                     <label>Name</label>
-                                    <input type="text" />
+                                     <input type="text" name="parent_name"/> {/* /// */}
                                 </div>
                                 <div className="inputGroup">
                                     <label>Phone</label>
-                                    <input type="text" />
+                                    <input type="text" name="phone"/>  {/* /// */}
                                 </div>
                             </div>
-                            <div class="inputRow">
-                                <div class="inputGroup">
+                            <div className="inputRow">
+                                <div className="inputGroup">
                                     <label>Email</label> 
-                                    <input type="email"/>
+                                    <input type="email" name="email"/>  {/* /// */}
                                 </div>
-                                <div class="inputGroup">
+                                <div className="inputGroup">
                                     <label>Students name</label> 
-                                    <input type="text"/>
+                                    <input type="text" name="child_name"/>  {/* /// */}
                                 </div>
                             </div>
                             <label>Message</label>
-                            <textarea rows="5" placeholder="Your message here."></textarea>
+                            <textarea rows="5" placeholder="Your message here." name="message"></textarea>  {/* /// */}
                             <button type='submit'  id="btn">Send</button>
-                        </Form>
+                            {success &&
+                                        <Modal show={show} onHide={handleClose}>
+                                            <Modal.Header closeButton>
+                                                <Modal.Title>Your message was sent successfuly!</Modal.Title>
+                                            </Modal.Header>
+                                                <Modal.Body>We will get back to you soon. Thank you for reaching out to Soaring Eagle Educational Center.</Modal.Body>
+                                            <Modal.Footer>
+                                                <Button variant="secondary" onClick={handleClose}>
+                                                    Close
+                                                </Button>
+                                            </Modal.Footer>
+                                        </Modal>}
+                        </form>
                     </div>
 
                 <div className="divider"></div>
@@ -52,6 +87,8 @@ export const Contact_us = () => {
                     <h2>Reach us. </h2>
                         
                         <table>
+                            <tbody>
+
                             <tr>
                                 <td>Email:</td>
                                 <td>contact@seecntr.org</td>
@@ -66,26 +103,24 @@ export const Contact_us = () => {
                                     Kettle Falls, WA 99141</td>
                                 </tr>
                                 <tr>
-                                    <h5>Please visit our Facebook page @ https://www.facebook.com/SoaringEagleEdCenter</h5>
+                                    <td colSpan={3}>
+
+                                        Please visit our Facebook page @{' '} 
+                                        
+                                        <a href="https://www.facebook.com/SoaringEagleEdCenter" target="_blank">SoaringEagleEdCenter</a>
+
+                                    </td>
                                 </tr>
+                            </tbody>
                         </table>
                     </div>
                 </div>
 
             </div>
 
-
-
-
-
-        
         {/* </div> */}
 
-
         </>
-
-
- 
 
     )
 };
