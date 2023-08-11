2a70cce37a1a2c83946c70cb078ef477c5d99e5c
Michael McIntosh <michaeldmc58@gmail.com>
2023-08-10 22:46:34 -0700

    nav update to scroll

--- SEED_project/src/pages/HomePage.jsx
+++ SEED_project/src/pages/HomePage.jsx
@@ -1,29 +1,65 @@
 import { Canvas } from '@react-three/fiber'
 import * as THREE from 'three'
+import { useRef, useContext } from 'react'
+import { Link } from 'react-router-dom'
 //////////////////////////////////////////////
 import HomeScene from '../threeHomeScene'
 import "../Styles/HomeBase.css"
 import TheTeam from "./AboutSEED/theTeam";
 import About from "./AboutSEED/about";
+import "../Styles/Nav.css";
+// import Navbar from './Navbar'
 export const HomePage = () => {
+    const team = useRef(null);
+    const about = useRef(null);
+    const styles = {textDecoration: 'none', color: "#b9a892"};
+
 
     const created = ({scene}) => {
         scene.background = new THREE.Color('antiquewhite')
         console.log('created')
       }
     
+    const scrollToSection = (elementRef) => {
+        window.scrollTo({
+            top: elementRef.current.offsetTop,
+            behavior: 'smooth'
+        })
+    }
 
     return(
         <>
+        {/* /////////////////////////Navbar////////////////////// */}
+            <div className="navbar">
+            <h2>Soaring Eagle Education Center</h2>
+
+            <img src="/pictures/SoaringEagleLogo.png" alt="Logo"/>
+
+            <nav className="navLinks">
+                <Link to={'/'} style={styles}>Home</Link>
+                <Link onClick={() => scrollToSection(about)} style={styles}>About</Link>
+                <Link to={'funzone'} style={styles}>FunZone</Link>
+                <Link onClick={() => scrollToSection(team)} style={styles}>Contact</Link>
+
+            </nav>
+        </div>
+        {/* /////////////////////////Navbar////////////////////// */}
+
+
+
+            <Canvas onCreated={created} >
+                <HomeScene />
+            </Canvas>
+
+            <h1>Homepage</h1>
+
+            <div ref={about}>
+                <About />
+            </div>
+            <div ref={team}>
+                <TheTeam />
+            </div>
         
-        <Canvas onCreated={created} >
-            <HomeScene />
-        </Canvas>
-
-        <h1>Homepage</h1>
-
-        <About />
-        <TheTeam />
         </>
     );
 }
--- SEED_project/src/pages/Navbar.jsx
+++ SEED_project/src/pages/Navbar.jsx
@@ -1,25 +1,35 @@
-import { Link } from "react-router-dom";
 
-import HomePage from "./HomePage";
-import "../Styles/Nav.css"
 
-export const Navbar = () => {
-const styles = {textDecoration: 'none', color: "#b9a892"}
 
-    return (
-        <div className="navbar">
-            <h2>Soaring Eagle Education Center</h2>
 
-            <img src="/pictures/SoaringEagleLogo.png" alt="Logo"/>
+// may delete /////////////// discontinued use // navbar now in homepage.jsx //////////////////
 
-            <nav className="navLinks">
-                <Link to={'/'} style={styles}>Home</Link>
-                <Link to={'about'} style={styles}>About</Link>
-                <Link to={'funzone'} style={styles}>FunZone</Link>
-                <Link to={'?'} style={styles}>Contact</Link>
 
-            </nav>
-        </div>
-    )
-}
-export default Navbar;
+
+
+
+// import { Link } from "react-router-dom";
+// import "../Styles/Nav.css";
+
+// export const Navbar = () => {
+// const styles = {textDecoration: 'none', color: "#b9a892"};
+
+
+//     return (
+//         <div className="navbar">
+//             <h2>Soaring Eagle Education Center</h2>
+
+//             <img src="/pictures/SoaringEagleLogo.png" alt="Logo"/>
+
+//             <nav className="navLinks">
+//                 <Link to={'/'} style={styles}>Home</Link>
+//                 <Link onClick={() => scrollToSection(about)} style={styles}>About</Link>
+//                 <Link to={'funzone'} style={styles}>FunZone</Link>
+//                 <Link to={'?'} style={styles}>Contact</Link>
+
+//             </nav>
+//         </div>
+//     )
+// }
+// export default Navbar;
+
