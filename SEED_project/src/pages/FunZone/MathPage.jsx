3344f999d5163f56762afdf7a5bd2db4d96e157c
Michael McIntosh <michaeldmc58@gmail.com>
2023-08-17 11:01:10 -0700

    mathGame done needs backend code

--- SEED_project/src/pages/FunZone/MathPage.jsx
+++ SEED_project/src/pages/FunZone/MathPage.jsx
@@ -2,34 +2,25 @@ import { useEffect, useState } from "react";
 import './funZoneStyles/mathPage.css'
 
 
+
+
+
 export const MathPage = () => {
-    
-
-
-
-    /////////////////////////////////////////////////MathLogic Addition///////////////////////////////////////////////////
-    const [answer, setAnswer] = useState('');
-    const [resetText, setResetText] = useState('New Numbers')
+       
+    // const [newNumBtn, setNewNumBtn] = useState(false)
     const [text, setText] = useState('');
-    const [outcome, setOutcome] = useState('dns');
     const [multiplier, setMultiplier] = useState(10)
     const [x, setX] = useState(Math.floor(Math.random() * multiplier));
     const [y, setY] = useState(Math.floor(Math.random() * multiplier));
     const [z, setZ] = useState('');
-    const [tof, setToF] = useState(false);
-    const [win, setWin] = useState(false)
+    const [showBtn, setShowBtn] = useState(true);
+    const [replayBtn, setReplayBtn] = useState(false)
     const [count, setCount] = useState(0);
 
 
-    useEffect(() => {
-        console.log(win, x,y)
-        
-        console.log(multiplier)
-      
-    }, [tof,count]);
-
-    
-    
+  useEffect(() => {
+    console.log('multiplier =', multiplier);
+  },[multiplier])
 
     const addTwo = (x,y) => x + y;
 
@@ -37,71 +28,90 @@ export const MathPage = () => {
 
     const generateNewNumbers = () => {
         //if correct
-        if (win) {
-            setToF(false)
-        } else {
-            setCount(0);
-            setToF(false)
-        }
-        if(count >= 5){
-            setMultiplier(20);
+ 
+        if(count + 1 >= 15){
+            setMultiplier(100);
         } 
-        if ( count >= 10){
+        else if ( count + 1 >= 10){
             setMultiplier(50);
         }
-        if ( count >= 15){
-            setMultiplier(100);
+        else if ( count + 1 >= 5){
+            setMultiplier(20);
         }
-        if ( count >= 25){
-            setMultiplier(10);
-            /////////// add in multiplication
-        }
+       
+        //     /////////// add in multiplication
         // if count = 50 game over user wins
         
+        setShowBtn(true)
+        // setNewNumBtn(false);
 
-        setAnswer('');
         setText('');
         setX(Math.floor(Math.random() * multiplier));
         setY(Math.floor(Math.random() * multiplier));
         setZ('');
-        setOutcome('dns');
-        
-        
-        
-        //if incorrect
-        
     }
     
     const correctAnswer = () => {
         setZ(text);
         setCount(count + 1);
-        setOutcome('win');
-        setWin(true);
-        setToF(true);
-        setResetText('New Numbers')
-    }
+        setShowBtn(false);
+        // setNewNumBtn(true);
+        setTimeout(() => {
+            generateNewNumbers();
+        }, 1250);
+    };
     
     const wrongAnswer = () => {
-        setOutcome('loss');
-        setWin(false)
         setZ( ' sorry wrong answer.');
-        setResetText('Start over')
-        setToF(true);
+        setShowBtn(false);
+        setReplayBtn(true)
+    }
+
+    const resetGame = () => {
+        setCount(0);
+        setMultiplier(10);
+        setShowBtn(true);
+        setReplayBtn(false)
+        setX(Math.floor(Math.random() * 10));
+        setY(Math.floor(Math.random() * 10));
+        setZ('');
+        setText('');
     }
 
     return (
         
         <>
-        <div id="mathGame">
-            <div className={outcome}>
-                <h2>Correct answers: {count}</h2>
-                <h1>{x} + {y} = {z}</h1>
-                <input type="text" placeholder="answer" value={text} onChange={(e) => setText(e.target.value)} />
-                <button hidden={tof} onClick={() => setAnswer(mathGame() ? correctAnswer  : wrongAnswer)}>Check Answer</button>
-                <button onClick={generateNewNumbers}>{resetText}</button>
+        <div className="background">
+
+            <div className="mathGame">
+                
+                    <div className="answerCount">
+                        Correct answers: {count}
+                    </div>
+                    <div className="equation">
+                        {x} + {y} = {z}
+                    </div>
+                    { !replayBtn
+                        &&
+                    <input className="inputMath" type="text" placeholder="answer" value={text}
+                     onChange={(e) => setText(e.target.value)}
+                     onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                                if (mathGame()) {
+                                    correctAnswer();
+                                } else {
+                                    wrongAnswer();
+                                }
+                            
+                        }
+                     }} />}
+                    {showBtn && <button className="button" onClick={mathGame() ? correctAnswer  : wrongAnswer}>Check Answer</button>}
+                   {/* {newNumBtn && <button className="button" onClick={generateNewNumbers}>New Numbers</button>} */}
+                   {replayBtn && <button className="button" onClick={resetGame} >Play Again?</button> }
+                
             </div>
+            
         </div>
-        
         </>
     )
 }
