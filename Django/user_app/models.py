28909b8d27ba603eb129d814dc17d3f1ee36a6f5
Michael McIntosh <michaeldmc58@gmail.com>
2023-08-21 09:26:09 -0700

    restarting backend

--- Django/user_app/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class Web_user(AbstractUser):
-    email = models.EmailField(unique=True)
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
